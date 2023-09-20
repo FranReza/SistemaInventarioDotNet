06b59205c1260d2f16cebd7d92e045a8716d8178
Fran.js <59714199+FranReza@users.noreply.github.com>
2023-09-20 10:54:15 -0600

    Configuracion del proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre de la bodega es requerida")]
+        [MaxLength(60, ErrorMessage = "Nombre debe ser maximo 60 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Descripcion es requerida")]
+        [MaxLength(100, ErrorMessage = "Descripcion debe ser maximo 100 caracteres")]
+        public string Descripcion { get; set;}
+
+        [Required(ErrorMessage = "agregalo perro")]
+        public string Estado { get; set;}
+    }
+}
